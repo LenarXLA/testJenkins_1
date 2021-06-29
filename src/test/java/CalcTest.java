7bb8fecc2000366d64c9f4d4c26d04939d169090
Lenar <leonruscraft@gmail.com>
2021-06-29 15:31:42 +0300

    feat: add 3 test method

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -11,8 +11,12 @@ public class CalcTest {
     public void testSum1() throws Exception {
         Assert.assertEquals(5, calc.sum(2,3));
     }
-    @Test(description = "Test 2 - failure")
+    @Test(description = "Test 2")
     public void testSum2() throws Exception {
         Assert.assertEquals(7, calc.sum(3,4));
+    }
+    @Test(description = "Test 3")
+    public void testSum3() throws Exception {
+        Assert.assertEquals(10, calc.sum(5,5));
     }
 }
